155c31c291f41b3dbe4f59687706a90733afe4fd
Alvertos <fiorantis-al@hotmail.com>
2020-11-22 20:10:12 +0000

    fixing backend imports so the app can work now

--- backEnd/controlers/Users/login.js
+++ backEnd/controlers/Users/login.js
@@ -1,73 +1,73 @@
+const jwt = require("jsonwebtoken");
 
-const jwt = require('jsonwebtoken')
+const HttpError = require("../../error/http-error");
 
-const HttpError = require('../error/http-error')
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/user')
-const bcrypt = require('bcryptjs')
-
+// Na thimame na kano export to const pou ftiaxno sto telos tou file
 //to functinality pou trexei otan thelo na kano login
-export const login = async (req, res, next) => {
-  const { email, password } = req.body
-  console.log('Login...')
-  let existingUser
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log("Login...");
+  let existingUser;
 
   try {
-    existingUser = await User.findOne({ email: email })
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
-      'Logging in failed, please try again later.',
-      500,
-    )
-    return next(error)
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
   // prota tsekaro an exo to user
   if (!existingUser) {
     const error = new HttpError(
-      'Invalid credentials, could not log you in.',
-      401,
-    )
-    return next(error)
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
   }
 
   // an exo to user tsekaro an to incomign email teriazei me to eimail pou exo sto database EXo to isVlaidPaswoard declare ekso apo to try catch gia na boro na to xirismopiso kai sto ifstmane pio kato
-  let isValidPassword = false
+  let isValidPassword = false;
   try {
     // to passwriod mesa sto compair eiani to incomniv passowrd kai tsekaro an teriazei me to passwrod pou exo sto database pou eian ito existingUser.passwrod afto giranei true false kai to kano store sto isValidPassowrd dummy varialbe
-    isValidPassword = await bcrypt.compare(password, existingUser.password)
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
   } catch (err) {
     // to erorr edo mesa den eiani oti dne teriazoun ta password ala oti eixa kapio texniko provlima gia afto kano meta pio kato to ifasmetns sto isValidPassord gia na do an odos teriazoun ta passowrd i oxi
     const error = new HttpError(
-      'Could not log you in, please check your credentials and try again.',
-      500,
-    )
-    return next(error)
+      "Could not log you in, please check your credentials and try again.",
+      500
+    );
+    return next(error);
   }
   // an eianai false simei oti exo lathos password kai kano send piso error
 
   if (!isValidPassword) {
     const error = new HttpError(
-      'Invalid credentials, could not log you in.',
-      401,
-    )
-    return next(error)
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
   }
 
   // tora pou ksero oti to apssrod einai match thelo na kano generate to json token
-  let token
+  let token;
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      'supersecret_dont_share',
-      { expiresIn: '1h' },
-    )
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
   } catch (err) {
     const error = new HttpError(
-      'Logging in failed, please try again later.',
-      500,
-    )
-    return next(error)
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
   // an den eiani fasl ola kala kai ton kano loggedin
@@ -76,5 +76,7 @@ export const login = async (req, res, next) => {
     email: existingUser.email,
     token: token,
     image: existingUser.image,
-  })
-}
+  });
+};
+
+module.exports = login;
--- backEnd/controlers/Users/verifyToken.js
+++ backEnd/controlers/Users/verifyToken.js
@@ -1,40 +1,39 @@
+const jwt = require("jsonwebtoken");
 
-const jwt = require('jsonwebtoken')
+const HttpError = require("../../error/http-error");
 
-const HttpError = require('../error/http-error')
+const User = require("../../models/user");
 
-const User = require('../models/user')
-
-
-export const verifyToken = async (req, res, next) => {
-  const data = req.userData
-  console.log(data)
-  let existingUser
+// na thimame na kano export to const sto telos tou file
+const verifyToken = async (req, res, next) => {
+  const data = req.userData;
+  console.log(data);
+  let existingUser;
 
   try {
-    existingUser = await User.findOne({ email: data.email })
+    existingUser = await User.findOne({ email: data.email });
   } catch (err) {
     const error = new HttpError(
-      'Logging in failed, please try again later.',
-      500,
-    )
-    return next(error)
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
   // tora pou ksero oti to apssrod einai match thelo na kano generate to json token
-  let token
+  let token;
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      'supersecret_dont_share',
-      { expiresIn: '1h' },
-    )
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
   } catch (err) {
     const error = new HttpError(
-      'Logging in failed, please try again later.',
-      500,
-    )
-    return next(error)
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
   // an den eiani fasl ola kala kai ton kano loggedin
@@ -43,5 +42,7 @@ export const verifyToken = async (req, res, next) => {
     email: existingUser.email,
     token: token,
     image: existingUser.image,
-  })
-}
+  });
+};
+
+module.exports = verifyToken;
