186d4949a7cffd9e0d49b8fa8c15e5cbca8ebc22
Alvertos <fiorantis-al@hotmail.com>
2020-11-29 14:13:05 +0000

    adding a projectComponent

---
+++ frontend/src/components/ProjectComponent/ProjectComponent.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ThreeDots from "../../icons/ThreeDots";
+import ProjectIcon from "../../icons/ProjectIcon";
+import BookCover from "../../icons/BookCover";
+
+// NOTE: na thimitho na alakso to a tag se LINK me to react router
+function ProjectComponent(props) {
+  return (
+    <li class="project__item">
+      <a href="#" className="project__link focus--box-shadow">
+        <div className="project__wrapper">
+          {props.projectCategory == "external-project" ? (
+            <div className="project__element project__icon">
+              <div
+                className="icon icon--viking"
+                aria-label="Icon for the project 'Book cover design'"
+              >
+                <ProjectIcon />
+              </div>
+            </div>
+          ) : (
+            <div class="project__element project__icon">
+              <div
+                class="icon icon--rajah"
+                aria-label="Icon for the project 'Book cover design'"
+              >
+                <BookCover />
+              </div>
+            </div>
+          )}
+          <div className="project__element project__inform">
+            <span className="project__inform-name">{props.projectName}</span>
+          </div>
+          <div className="project__element project__photo">
+            <ul className="photo">
+              <li className="photo__item">
+                {/* <img src="" alt="Jessica's photo" /> */}
+              </li>
+            </ul>
+          </div>
+          <div className="project__element project__date">
+            <time className="date">{props.date}</time>
+          </div>
+          {props.projectStatus == "in-work" ? (
+            <div className="project__element project__status">
+              <span className="status status--in-work">
+                {props.projectStatus}
+              </span>
+            </div>
+          ) : (
+            <div class="project__element project__status">
+              <span class="status status--published">
+                {props.projectStatus}
+              </span>
+            </div>
+          )}
+          <div className="project__element project__setting">
+            <button
+              className="setting setting--rotate focus--box-shadow"
+              type="button"
+            >
+              <ThreeDots
+                view_Box="0 0 515.555 515.555"
+                enable_background={"new 0 0 515.555 515.555"}
+                svg_height={"512"}
+                svg_width={"512"}
+              />
+            </button>
+          </div>
+        </div>
+      </a>
+    </li>
+  );
+}
+
+export default ProjectComponent;
--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -2,26 +2,24 @@ import React from "react";
 import "./Dashboard.css";
 import Settings from "../../icons/Settings";
 import PlusSign from "../../icons/PlusSign";
-import ProjectIcon from "../../icons/ProjectIcon";
-import BookCover from "../../icons/BookCover";
-import ThreeDots from "../../icons/ThreeDots";
+import ProjectComponent from "../../components/ProjectComponent/ProjectComponent";
 
 function Dashboard() {
   return (
-    <div class="wrapper">
-      <section class="section">
-        <header class="section__header">
-          <h2 class="section__title">Projects</h2>
-          <div class="section__control">
+    <div className="wrapper">
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Projects</h2>
+          <div className="section__control">
             <button
-              class="section__button focus--box-shadow"
+              className="section__button focus--box-shadow"
               type="button"
               aria-label="Filter projects"
             >
               <Settings />
             </button>
             <button
-              class="section__button section__button--painted focus--box-shadow"
+              className="section__button section__button--painted focus--box-shadow"
               type="button"
               aria-label="Add New project"
             >
@@ -29,150 +27,25 @@ function Dashboard() {
             </button>
           </div>
         </header>
-        <ul class="project">
-          <li class="project__item">
-            <a href="#" class="project__link focus--box-shadow">
-              <div class="project__wrapper">
-                <div class="project__element project__icon">
-                  <div
-                    class="icon icon--viking"
-                    aria-label="Icon of the 'Showcase Design' project"
-                  >
-                    <ProjectIcon />
-                  </div>
-                </div>
-                <div class="project__element project__inform">
-                  <span class="project__inform-name">Product presentation</span>
-                </div>
-                <div class="project__element project__photo">
-                  <ul class="photo">
-                    <li class="photo__item">
-                      <span class="photo__substrate">+2</span>
-                    </li>
-                    <li class="photo__item">
-                      {/* <img src="" alt="Jack's photo" /> */}
-                    </li>
-                    <li class="photo__item">
-                      {/* <img src="" alt="Jessica's photo" /> */}
-                    </li>
-                  </ul>
-                </div>
-                <div class="project__element project__date">
-                  <time class="date" datetime="2020-05-05T10:00:00">
-                    05 May, 2020
-                  </time>
-                </div>
-                <div class="project__element project__status">
-                  <span class="status status--published">Published</span>
-                </div>
-                <div class="project__element project__setting">
-                  <button
-                    class="setting setting--rotate focus--box-shadow"
-                    type="button"
-                  >
-                    <ThreeDots
-                      view_Box="0 0 515.555 515.555"
-                      enable_background={"new 0 0 515.555 515.555"}
-                      svg_height={"512"}
-                      svg_width={"512"}
-                    />
-                  </button>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="project__item">
-            <a href="#" class="project__link focus--box-shadow">
-              <div class="project__wrapper">
-                <div class="project__element project__icon">
-                  <div
-                    class="icon icon--rajah"
-                    aria-label="Icon for the project 'Book cover design'"
-                  >
-                    <BookCover />
-                  </div>
-                </div>
-                <div class="project__element project__inform">
-                  <span class="project__inform-name">Book cover design</span>
-                </div>
-                <div class="project__element project__photo">
-                  <ul class="photo">
-                    <li class="photo__item">
-                      {/* <img src="" alt="Jessica's photo" /> */}
-                    </li>
-                  </ul>
-                </div>
-                <div class="project__element project__date">
-                  <time class="date" datetime="2020-05-05T10:00:00">
-                    13 February, 2020
-                  </time>
-                </div>
-                <div class="project__element project__status">
-                  <span class="status status--published">Published</span>
-                </div>
-                <div class="project__element project__setting">
-                  <button
-                    class="setting setting--rotate focus--box-shadow"
-                    type="button"
-                  >
-                    <ThreeDots
-                      view_Box="0 0 515.555 515.555"
-                      enable_background={"new 0 0 515.555 515.555"}
-                      svg_height={"512"}
-                      svg_width={"512"}
-                    />
-                  </button>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="project__item">
-            <a href="#" class="project__link focus--box-shadow">
-              <div class="project__wrapper">
-                <div class="project__element project__icon">
-                  <div
-                    class="icon icon--viking"
-                    aria-label="Icon for the project 'Book cover design'"
-                  >
-                    <ProjectIcon />
-                  </div>
-                </div>
-                <div class="project__element project__inform">
-                  <span class="project__inform-name">
-                    Logo redesign for eyeglass store
-                  </span>
-                </div>
-                <div class="project__element project__photo">
-                  <ul class="photo">
-                    <li class="photo__item">
-                      {/* <img src="" alt="Jessica's photo" /> */}
-                    </li>
-                  </ul>
-                </div>
-                <div class="project__element project__date">
-                  <time class="date" datetime="2020-05-05T10:00:00">
-                    01 February, 2020
-                  </time>
-                </div>
-                <div class="project__element project__status">
-                  <span class="status status--in-work">In work</span>
-                </div>
-                <div class="project__element project__setting">
-                  <button
-                    class="setting setting--rotate focus--box-shadow"
-                    type="button"
-                  >
-                    <ThreeDots
-                      view_Box="0 0 515.555 515.555"
-                      enable_background={"new 0 0 515.555 515.555"}
-                      svg_height={"512"}
-                      svg_width={"512"}
-                    />
-                  </button>
-                </div>
-              </div>
-            </a>
-          </li>
+        <ul className="project">
+          <ProjectComponent
+            projectCategory="external-project"
+            date="05 May, 2020"
+            projectName="Product presentation"
+            projectStatus="published"
+          />
+          <ProjectComponent
+            projectCategory="internal-project"
+            date="1 January 2020"
+            projectName="Internal project"
+            projectStatus="published"
+          />
+          <ProjectComponent
+            projectCategory="external-project"
+            date="1 February 2020"
+            projectName="To diko mou to project"
+            projectStatus="in-work"
+          />
         </ul>
       </section>
     </div>
